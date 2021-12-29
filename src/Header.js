8355873d3f2ef98a67e8d9a092c1af6cbd46c2b0
max80713 <max80713@hotmail.com>
2021-12-29 17:29:03 +0800

    Remove algolia search

--- src/Header.js
+++ src/Header.js
@@ -1,45 +1,15 @@
-import { Menu, Search } from 'semantic-ui-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import React from 'react';
-import algolia from './utils/algolia';
 
 import { auth } from './utils/firebase';
 import { signOut } from 'firebase/auth';
 
 function Header({ user }) {
-  const navigate = useNavigate();
-  const [inputValue, setInputValue] = React.useState('');
-  const [results, setResults] = React.useState([]);
-
-  async function onSearchChange(e, { value }) {
-    setInputValue(value);
-
-    const result = await algolia.search(value);
-    const searchResults = result.hits.map((hit) => ({
-      title: hit.title,
-      description: hit.content,
-      id: hit.objectID,
-    }));
-    setResults(searchResults);
-  }
-
-  function onResultSelect(e, { result }) {
-    navigate(`/posts/${result.id}`);
-  }
-
   return (
     <Menu>
       <Menu.Item as={Link} to="/posts">
         Social Cool
-      </Menu.Item>
-      <Menu.Item>
-        <Search
-          value={inputValue}
-          onSearchChange={onSearchChange}
-          results={results}
-          noResultsMessage="找不到相關文章"
-          onResultSelect={onResultSelect}
-        />
       </Menu.Item>
       <Menu.Menu position="right">
         {user ? (
